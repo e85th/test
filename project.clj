4e070999f1d00cdeda874f6f4648732f60d858bb
Amar Mehta <xfthhxk@gmail.com>
2017-02-13 11:01:38 -0500

    add edn, transit formats for api requests

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject e85th/test "0.1.1"
+(defproject e85th/test "0.1.2"
   :description "Testing related utilities."
   :url "http://github.com/e85th/test"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-mock "0.3.0"]
+                 [com.cognitect/transit-clj "0.8.297"]
                  [cheshire "5.6.3"]]
 
 
--- src/clj/e85th/test/http.clj
+++ src/clj/e85th/test/http.clj
@@ -1,14 +1,42 @@
 (ns e85th.test.http
   (:require [e85th.test.util :as u]
             [ring.mock.request :as mock]
+            [cognitect.transit :as transit]
+            [clojure.edn :as edn]
+            [clojure.string :as str]
             [cheshire.core :as json])
-  (:import [java.io InputStream]))
+  (:import [java.io InputStream ByteArrayInputStream ByteArrayOutputStream]))
 
 
 (def ^:private routes nil)
 
 (def ^{:doc "Request auth header."}
   auth-header "authorization")
+
+(def http-success-status-codes #{200 201 202 203 204})
+
+(defn success?
+  "Answers if the input status-code is a http success code."
+  [status-code]
+  (http-success-status-codes status-code))
+
+(defn content-type->name
+  [s]
+  (let [s (str/lower-case (or s ""))]
+    (cond
+      (str/includes? s "application/json") :json
+      (str/includes? s "application/edn")  :edn
+      :else :other)))
+
+(defn json?
+  "Answers true if the content-type is application/json. Ignores case, handles nil."
+  [content-type]
+  (= :json (content-type->name content-type)))
+
+(defn edn?
+  "Answers true if the content-type is application/json. Ignores case, handles nil."
+  [content-type]
+  (= :edn (content-type->name content-type)))
 
 (defn init!
   "Call this first before using any other functions."
@@ -43,6 +71,53 @@
          (mock/content-type "application/json")
          (update-in [:headers] merge headers)))))
 
+(defn edn-request
+  "Creates and returns a new mock ring request map with content-type set to application/edn."
+  ([method uri]
+   (edn-request method uri {}))
+  ([method uri params]
+   (edn-request method uri params {}))
+  ([method uri params headers]
+   (assert (keyword? method))
+   (assert (string? uri))
+   (assert (or (map? params)
+               (sequential? params)))
+   (assert (map? headers))
+   (let [params (if (= :get method) params (pr-str params))]
+     (-> (mock/request method uri params)
+         (mock/content-type "application/edn")
+         (update-in [:headers] merge headers)))))
+
+(defn transit-encode
+  [x]
+  (let [out (ByteArrayOutputStream. 4096)
+        writer (transit/writer out :json)]
+    (transit/write writer x)
+    (.toString out)))
+
+(defn transit-decode
+  [^String s]
+  (let [in (ByteArrayInputStream. (.getBytes s))
+        reader (transit/reader in :json)]
+    (transit/read reader)))
+
+(defn transit-request
+  "Creates and returns a new mock ring request map with content-type set to application/edn."
+  ([method uri]
+   (transit-request method uri {}))
+  ([method uri params]
+   (transit-request method uri params {}))
+  ([method uri params headers]
+   (assert (keyword? method))
+   (assert (string? uri))
+   (assert (or (map? params)
+               (sequential? params)))
+   (assert (map? headers))
+   (let [params (if (= :get method) params (transit-encode params))]
+     (-> (mock/request method uri params)
+         (mock/content-type "application/transit+json")
+         (update-in [:headers] merge headers)))))
+
 (defn json-response-body-as-edn
   "Parse the ring response's body and return it as a Clojure data structure."
   [{:keys [body] :as response}]
@@ -51,10 +126,33 @@
   (let [body (if (instance? InputStream body) (slurp body) body)]
     (json/parse-string body true)))
 
+(defn edn-response-body-as-edn
+  "Parse the ring response's body and return it as a Clojure data structure."
+  [{:keys [body] :as response}]
+  ;; Check for InputStream because api-call uses the routes directly and serialization
+  ;; doesn't happen in testing with undertow anyway
+  (let [body (if (instance? InputStream body) (slurp body) body)]
+    (edn/read-string body)))
 
-(def ^{:doc "Takes a ring response and converts it to a tuple [status-code body headers]"}
-  response->tuple
+(defn transit-response-body-as-edn
+  "Parse the ring response's body and return it as a Clojure data structure."
+  [{:keys [body] :as response}]
+  ;; Check for InputStream because api-call uses the routes directly and serialization
+  ;; doesn't happen in testing with undertow anyway
+  (let [body (if (instance? InputStream body) (slurp body) body)]
+    (transit-decode body)))
+
+(def ^{:doc "Takes a ring json response and converts it to a tuple [status-code body headers]"}
+  json-response->tuple
   (juxt :status json-response-body-as-edn :headers))
+
+(def ^{:doc "Takes a ring edn response and converts it to a tuple [status-code body headers]"}
+  edn-response->tuple
+  (juxt :status edn-response-body-as-edn :headers))
+
+(def ^{:doc "Takes a ring transit response and converts it to a tuple [status-code body headers]"}
+  transit-response->tuple
+  (juxt :status transit-response-body-as-edn :headers))
 
 (defn make-api-caller
   "Returns a function that can be used to make api-calls with the request-modifier
@@ -63,24 +161,33 @@
    [request]
    [method :- s/Keyword uri :- s/Str]
    [method :- s/Keyword uri :- s/Str params :- {s/Keyword s/Any}]
-   [method :- s/Keyword uri :- s/Str params :- {s/Keyword s/Any} headers :- {s/Keyword s/Any}]
-  "
+   [method :- s/Keyword uri :- s/Str params :- {s/Keyword s/Any} headers :- {s/Keyword s/Any}]"
+  [make-request-fn request-modifier response-processor]
+  (fn f
+    ([request]
+     (response-processor (routes (request-modifier request))))
+    ([method uri]
+     (f (make-request-fn method uri)))
+    ([method uri params]
+     (f (make-request-fn method uri params)))
+    ([method uri params headers]
+     (f (make-request-fn method uri params headers)))))
+
+
+(defn make-json-api-caller
+  ([]
+   (make-json-api-caller identity))
   ([request-modifier]
-   (make-api-caller request-modifier response->tuple))
+   (make-api-caller json-request request-modifier json-response->tuple)))
 
-  ([request-modifier response-processor]
-   (fn f
-     ([request]
-      (response-processor (routes (request-modifier request))))
-     ([method uri]
-      (f (json-request method uri)))
-     ([method uri params]
-      (f (json-request method uri params)))
-     ([method uri params headers]
-      (f (json-request method uri params headers))))))
+(defn make-edn-api-caller
+  ([]
+   (make-edn-api-caller identity))
+  ([request-modifier]
+   (make-api-caller edn-request request-modifier edn-response->tuple)))
 
-(def ^{:doc "Calls an endpoint and returns the .
-             The status-code is an int, response-body is a Clojure data structure.
-             Executes a json request to the api endpoint.
-             eg: (api-call :get \"/foo\" {:a 1 :b 2})"}
-  api-call (make-api-caller identity))
+(defn make-transit-api-caller
+  ([]
+   (make-transit-api-caller identity))
+  ([request-modifier]
+   (make-api-caller transit-request request-modifier transit-response->tuple)))
