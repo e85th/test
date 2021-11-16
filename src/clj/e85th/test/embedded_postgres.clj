641b7202dc32bb254f44279681bad8b4d59e5699
Amar Mehta <xfthhxk@gmail.com>
2021-11-15 22:46:00 -0500

    upgrade deps, adapt flyway changes

--- src/clj/e85th/test/embedded_postgres.clj
+++ src/clj/e85th/test/embedded_postgres.clj
@@ -1,13 +1,13 @@
 (ns e85th.test.embedded-postgres
-  (:require [clojure.spec.alpha :as s]
-            [com.stuartsierra.component :as component])
+  (:require [com.stuartsierra.component :as component])
   (:import [javax.sql DataSource]
            [org.flywaydb.core Flyway]
+           [org.flywaydb.core.api.configuration FluentConfiguration]
            [com.opentable.db.postgres.embedded DatabasePreparer PreparedDbProvider]))
 
 (defn set-data-source
-  [^Flyway flyway ^DataSource ds]
-  (.setDataSource flyway ds))
+  [^FluentConfiguration config ^DataSource ds]
+  (.dataSource config ds))
 
 (defn migrate
   [^Flyway flyway]
@@ -16,22 +16,22 @@
 (defn set-locations
   "Sets the locations where flyway seaches for migrations.
    locations is a seq of strings signifying directories on disk."
-  [^Flyway flyway locations]
-  (.setLocations flyway (into-array locations)))
+  [^FluentConfiguration config locations]
+  (.locations config (into-array locations)))
 
 
 (defn set-placeholders
-  [^Flyway flyway placeholder-map]
-  (.setPlaceholders flyway placeholder-map))
+  [^FluentConfiguration config placeholder-map]
+  (.placeholders config placeholder-map))
 
 (defn set-filesystem-locations
   "Sets the filesystem locations where migration files are found.
    The locations should be `String` and not be prefixed with `filesystem:`.
    This function will handle that."
-  [^Flyway flyway locations]
+  [^FluentConfiguration config locations]
   (->> locations
        (map (partial str "filesystem:"))
-       (set-locations flyway)))
+       (set-locations config)))
 
 (defn create-data-source
   [^PreparedDbProvider db-provider]
@@ -42,14 +42,15 @@
   `pre-migrate-fn` and `post-migrate-fn` are single arity functions that
    take a `DataSource` and are called prior to and after the migrations
    have run."
-  [^Flyway flyway pre-migrate-fn post-migrate-fn]
+  [^FluentConfiguration config pre-migrate-fn post-migrate-fn]
   (reify DatabasePreparer
     (prepare [this datasource]
       (pre-migrate-fn datasource)
 
-      (doto flyway
-        (set-data-source datasource)
-        (migrate))
+      (-> config
+          (set-data-source datasource)
+          (.load)
+          (.migrate))
 
       (post-migrate-fn datasource))))
 
@@ -58,21 +59,21 @@
   "Creates a new `PreparedDbProvider` which allows for migrations to be run once
   into Postgres' templateDB. Subsequent databases are generated from templateDB and
   are very fast."
-  [^Flyway flyway pre-migrate-fn post-migrate-fn]
-  (-> flyway
+  [^FluentConfiguration config pre-migrate-fn post-migrate-fn]
+  (-> config
       (database-preparer pre-migrate-fn post-migrate-fn)
       PreparedDbProvider/forPreparer))
 
 (defprotocol IFreshDataSource
   (fresh-data-source [this] "Creates and returns a brand new datasource."))
 
-(defrecord Db [flyway pre-migrate-fn post-migrate-fn]
+(defrecord Db [flyway-config pre-migrate-fn post-migrate-fn]
   component/Lifecycle
   (start [this]
     (if (:db-provider this)
       this
       (-> this
-          (assoc :db-provider (prepared-db-provider flyway pre-migrate-fn post-migrate-fn))
+          (assoc :db-provider (prepared-db-provider flyway-config pre-migrate-fn post-migrate-fn))
           fresh-data-source)))
 
   (stop [this]
@@ -83,14 +84,18 @@
     (assoc this :datasource (-> this :db-provider create-data-source))))
 
 
+(def no-op (constantly nil))
+
 (defn new-db
   "Creates a db with filesystem locations where migrations are found.
   See `database-preparer` for docs on `pre-migrate-fn` and `post-migrate-fn`."
-  ([flyway-init-fn]
-   (new-db flyway-init-fn (constantly nil) (constantly nil)))
-  ([flyway-init-fn pre-migrate-fn]
-   (new-db flyway-init-fn pre-migrate-fn (constantly nil)))
-  ([flyway-init-fn pre-migrate-fn post-migrate-fn]
-   (map->Db {:flyway (flyway-init-fn (Flyway.))
-             :pre-migrate-fn pre-migrate-fn
-             :post-migrate-fn post-migrate-fn})))
+  [{:keys [init-fn pre-migrate-fn post-migrate-fn
+           config]
+    :or {init-fn identity}}]
+  (let [{:keys [placeholders locations]} config
+        config (Flyway/configure)]
+    (when locations (set-locations config locations))
+    (when placeholders (set-placeholders config placeholders))
+    (map->Db {:flyway-config (init-fn config)
+              :pre-migrate-fn pre-migrate-fn
+              :post-migrate-fn post-migrate-fn})))
